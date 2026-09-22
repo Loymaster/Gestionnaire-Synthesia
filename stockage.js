331fef7fef2d70def68c7d1f405d1192af4ee885
Loymaster <lois.kerrouche@gmail.com>
2026-09-22 14:26:41 +0200

    Update stockage.js

--- stockage.js
+++ stockage.js
@@ -17,7 +17,7 @@
   /* ═══════════════════════════════════════════════════════════════════
      À RENSEIGNER : adresse du déploiement Apps Script (se termine par /exec)
      ═══════════════════════════════════════════════════════════════════ */
-  var API = "";
+  var API = "https://script.google.com/macros/s/AKfycbwB2YirREvnxYH8SLw6jJZ-KOQ0YAy_8Z7Kqsw3qWD5leiUUglQSPju-YXb87PW_5MRIw/exec";
   /* Exemple :
      var API = "https://script.google.com/macros/s/AKfycb…/exec";
      Laisser vide fait fonctionner l'outil en mode local (démonstration). */
